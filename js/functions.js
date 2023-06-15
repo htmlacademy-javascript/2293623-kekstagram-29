6c10a446b7271130a598d90274238421f1570982
Елизавета Горохова <gorohova.liz@yandex.ru>
2023-06-15 18:32:58 +0300

    Нужно больше функций

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+// Функция для проверки длины строки.
+// проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и
+// false, если строка длиннее. Эта функция нам пригодится для валидации формы
+
+// let checkingStringLength = function (stringLength, maxLength) {
+//   if (stringLength.length <= maxLength) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// console.log(checkingStringLength('проверяемая строка', 20));
+
+let determinePalindrom = function (string) {
+  let newText = string.replaceAll('', '');
+  let reverseString = '';
+  for (let i = newText.length - 1; i >= 0; i = i - 1) {
+    reverseString += newText.at(i);
+  }
+  if (reverseString === newText) {
+    return 'Это палиндром';
+  }
+  return 'Это не палиндром';
+};
+
+console.log(determinePalindrom('топот'));
